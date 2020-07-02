2fd2c97f792d13b9dd5af9b29c60476f0da15318
irqize <irqize@gmail.com>
2020-07-02 19:55:51 +0200

    Added redux to the client.

--- client/index.js
+++ client/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./src/App";
-import "./styles.css";
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById("app"));
+import "./styles.css";
+import store from './store'
+
+ReactDOM.render((
+    <Provider store={store}>
+        <App />
+    </Provider>
+), document.getElementById("app"));
---
+++ client/store/rootReducer.js
@@ -0,0 +1,14 @@
+import { combineReducers } from 'redux'
+
+const initialState = {
+
+}
+
+export default function (state = initialState, action) {
+    switch (action.type) {
+
+
+        default:
+            return state;
+    }
+}
